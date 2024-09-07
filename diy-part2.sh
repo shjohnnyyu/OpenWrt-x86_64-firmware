3f1435f02e4103b904195306d68838ae57447b65
shjohnnyyu <1045995861@qq.com>
2024-09-07 22:30:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,9 @@ sed -i 's/192.168.100.1/192.168.50.5/g' package/base-files/files/bin/config_gene
 
 # Modify hostname
 sed -i 's/OpenWrt/YuJRouter/g' package/base-files/files/bin/config_generate
+
+#Add
+sed -i 's/root::0:0:99999:7:::/root:$1$TEkD9W7A$3GrY7.fWsVOa4OJ1d/FXm/:18520:0:99999:7:::/g' /etc/shadow
+sed -i '2a\ifconfig eth1 192.168.1.3 netmask 255.255.255.0 broadcast 192.168.1.255' /etc/rc.local
+sed -i '3a\iptables -I forwarding_rule -d 192.168.1.1 -j ACCEPT' /etc/rc.local
+sed -i '4a\iptables -t nat -I postrouting_rule -d 192.168.1.1 -j MASQUERADE' /etc/rc.local
