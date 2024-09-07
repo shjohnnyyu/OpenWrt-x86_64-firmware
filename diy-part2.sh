b59fd442cb4ccbe7f25590b08f2bb86a91a089e9
shjohnnyyu <1045995861@qq.com>
2024-09-08 02:01:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.100.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
@@ -19,9 +19,9 @@ sed -i 's/192.168.100.1/192.168.50.5/g' package/base-files/files/bin/config_gene
 # Modify hostname
 sed -i 's/OpenWrt/YuJRouter/g' package/base-files/files/bin/config_generate
 
-#Add
-#sed -i 's/root::0:0:99999:7:::/root:$1$TEkD9W7A$3GrY7.fWsVOa4OJ1d/FXm/:18520:0:99999:7:::/g' /etc/shadow
-#sed -i '2a\ifconfig eth1 192.168.1.3 netmask 255.255.255.0 broadcast 192.168.1.255' /etc/rc.local
-#sed -i '3a\iptables -I forwarding_rule -d 192.168.1.1 -j ACCEPT' /etc/rc.local
-#sed -i '4a\iptables -t nat -I postrouting_rule -d 192.168.1.1 -j MASQUERADE' /etc/rc.local
-#echo "javedyu@outlook.com:*:1000:65534:javedyu@outlook.com:/var:/bin/false" >> /etc/passwd
+#设置密码
+echo "sed -i 's/root::0:0:99999:7:::/root:$1$TEkD9W7A$3GrY7.fWsVOa4OJ1d/FXm/:18520:0:99999:7:::/g' /etc/shadow" >> package/lean/default-settings/files/zzz-default-settings
+echo "sed -i '2a\ifconfig eth1 192.168.1.3 netmask 255.255.255.0 broadcast 192.168.1.255' /etc/rc.local" >> package/lean/default-settings/files/zzz-default-settings
+echo "sed -i '3a\iptables -I forwarding_rule -d 192.168.1.1 -j ACCEPT' /etc/rc.local" >> package/lean/default-settings/files/zzz-default-settings
+echo "sed -i '4a\iptables -t nat -I postrouting_rule -d 192.168.1.1 -j MASQUERADE' /etc/rc.local" >> package/lean/default-settings/files/zzz-default-settings
+echo "echo "javedyu@outlook.com:*:1000:65534:javedyu@outlook.com:/var:/bin/false" >> /etc/passwd " >> package/lean/default-settings/files/zzz-default-settings
